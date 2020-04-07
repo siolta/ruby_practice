f254eebffc98f902f00816de541f84b750c5934d
Skylar Iolta <siolta@users.noreply.github.com>
2020-04-06 21:44:25 -0700

    ex 38-39

---
+++ LRTHW/rb_39.rb
@@ -0,0 +1,67 @@
+prompt = '-' * 10
+
+states = {
+    'Oregon' => 'OR',
+    'Florida' => 'FL',
+    'Calfornia' => 'CA',
+    'New York' => 'NY',
+    'Michigan' => 'MI'
+}
+
+# create a basic set of states and some cities in them
+cities = {
+    'CA' => 'San Francisco',
+    'MI' => 'Detroit',
+    'FL' => 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# puts out some cities
+puts prompt
+puts "NY State has: #{cities['NY']}"
+puts "OR State has: #{cities['OR']}"
+
+# puts some states
+puts prompt
+puts "Michigan's abbreviation is: #{states['Michigan']}"
+puts "Florida's abbreviation is: #{states['Florida']}"
+
+# do it by using the state then cities hash
+puts prompt
+puts "Michigan has: #{cities[states['Michigan']]}"
+puts "Florida has: #{cities[states['Florida']]}"
+
+# puts every state abbreviation
+puts prompt
+states.each do |state, abbrev|
+    puts "#{state} is abbreviated #{abbrev}"
+end
+
+# puts every city in state
+puts prompt
+cities.each do |abbrev, city|
+    puts "#{abbrev} has the city #{city}"
+end
+
+# now do both at the same time
+puts prompt
+states.each do |state, abbrev|
+    city = cities[abbrev]
+    puts "#{state} is abbreviated #{abbrev} and has city #{city}"
+end
+
+puts prompt
+# by default ruby says "nil" when something isn't in there
+state = states['Texas']
+
+if !state
+    puts "Sorry, no Texas."
+end
+
+# default values using ||= with the nil result
+city = cities['TX']
+city ||= 'Does Not Exist'
+puts "The city for the state 'TX' is: #{city}"
